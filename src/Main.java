bf21f4db687c5b09941146280f4b8cd4a7deb58e
CamBradford07 <152317249+CamBradford07@users.noreply.github.com>
2024-01-30 20:47:00 +0000

    Added textfields to get names

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,27 @@ import java.awt.event.ActionListener;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Play Yahtzee");
-frame.setSize(500,500);
-
+        frame.setSize(500,500);
+        JFrame applyNames = new JFrame("Enter Names");
+        applyNames.setSize(500,500);
+        
+      
         JPanel panel = new JPanel(new GridLayout(3,1));
+        JPanel getNames = new JPanel(new GridLayout(6,1));
 
         JLabel askPlayerCount = new JLabel("How many computers do you want to play against? 1-3");
         JTextField getPlayerCount = new JTextField();
         JButton submit = new JButton("Submit");
 
+      JTextField player1Name = new JTextField("Your Name");
+      JTextField player2Name = new JTextField("Computer 1");
+      JTextField player3Name = new JTextField("Computer 2");
+      JTextField player4Name = new JTextField("Computer 3");
+      
         submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int playercount = 1-1;
+                int playercount = 0;
 
                     try {
                         playercount = Integer.parseInt(getPlayerCount.getText());
