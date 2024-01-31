4730e6f37602fa7d9ccce9ca8b79028fc67d5ff0
CamBradford07 <152317249+CamBradford07@users.noreply.github.com>
2024-01-31 15:50:08 +0000

    added new class to play with

--- src/Main.java
+++ src/Main.java
@@ -34,13 +34,68 @@ public class Main {
       JLabel play1Ones = new JLabel("Ones: ");
       JLabel play1Twos = new JLabel("Twos: ");
       JLabel play1Threes = new JLabel("Threes: ");
-      JLabel play1Fours = new JLabel("FOurs: ");
+      JLabel play1Fours = new JLabel("Fours: ");
       JLabel play1Fives = new JLabel("Fives: ");
       JLabel play1Sixes = new JLabel("Sixes: ");
       JLabel play1SmallStraight = new JLabel("small Straight: ");
       JLabel play1LargeStraight = new JLabel("Large Straight: ");
+      JLabel play1Chance = new JLabel("Chance: ");
+      JLabel play1FourKind = new JLabel("Four of Kind: ");
+      JLabel play1ThreeKind = new JLabel("Three of Kind: ");
+      JLabel play1Yahtzee = new JLabel("Yahtzee: ");
+      JLabel play1FullHouse = new JLabel("Full House: ");
 
+      JLabel play2Name = new JLabel("");
+      JLabel play2Bonus = new JLabel("Bonus: ");
+      JLabel play2Ones = new JLabel("Ones: ");
+      JLabel play2Twos = new JLabel("Twos: ");
+      JLabel play2Threes = new JLabel("Threes: ");
+      JLabel play2Fours = new JLabel("Fours: ");
+      JLabel play2Fives = new JLabel("Fives: ");
+      JLabel play2Sixes = new JLabel("Sixes: ");
+      JLabel play2SmallStraight = new JLabel("small Straight: ");
+      JLabel play2LargeStraight = new JLabel("Large Straight: ");
+      JLabel play2Chance = new JLabel("Chance: ");
+      JLabel play2FourKind = new JLabel("Four of Kind: ");
+      JLabel play2ThreeKind = new JLabel("Three of Kind: ");
+      JLabel play2Yahtzee = new JLabel("Yahtzee: ");
+      JLabel play2FullHouse = new JLabel("Full House: ");
+
+      JLabel play3Name = new JLabel("");
+      JLabel play3Bonus = new JLabel("Bonus: ");
+      JLabel play3Ones = new JLabel("Ones: ");
+      JLabel play3Twos = new JLabel("Twos: ");
+      JLabel play3Threes = new JLabel("Threes: ");
+      JLabel play3Fours = new JLabel("Fours: ");
+      JLabel play3Fives = new JLabel("Fives: ");
+      JLabel play3Sixes = new JLabel("Sixes: ");
+      JLabel play3SmallStraight = new JLabel("small Straight: ");
+      JLabel play3LargeStraight = new JLabel("Large Straight: ");
+      JLabel play3Chance = new JLabel("Chance: ");
+      JLabel play3FourKind = new JLabel("Four of Kind: ");
+      JLabel play3ThreeKind = new JLabel("Three of Kind: ");
+      JLabel play3Yahtzee = new JLabel("Yahtzee: ");
+      JLabel play3FullHouse = new JLabel("Full House: ");
+
+      JLabel play4Name = new JLabel("");
+      JLabel play4Bonus = new JLabel("Bonus: ");
+      JLabel play4Ones = new JLabel("Ones: ");
+      JLabel play4Twos = new JLabel("Twos: ");
+      JLabel play4Threes = new JLabel("Threes: ");
+      JLabel play4Fours = new JLabel("Fours: ");
+      JLabel play4Fives = new JLabel("Fives: ");
+      JLabel play4Sixes = new JLabel("Sixes: ");
+      JLabel play4SmallStraight = new JLabel("small Straight: ");
+      JLabel play4LargeStraight = new JLabel("Large Straight: ");
+      JLabel play4Chance = new JLabel("Chance: ");
+      JLabel play4FourKind = new JLabel("Four of Kind: ");
+      JLabel play4ThreeKind = new JLabel("Three of Kind: ");
+      JLabel play4Yahtzee = new JLabel("Yahtzee: ");
+      JLabel play4FullHouse = new JLabel("Full House: ");
+      
       JButton submitNames = new JButton("Submit names");
+
+      
       
         submit.addActionListener(new ActionListener() {
             @Override
@@ -71,7 +126,7 @@ public class Main {
             }
         });
 
-        
+        //submitNames.addActionListener();
       
         panel.add(askPlayerCount);
         panel.add(getPlayerCount);
