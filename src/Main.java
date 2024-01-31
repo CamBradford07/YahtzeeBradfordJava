9c71632c2f33ec1bd78fabcd5bb5756afff8e8ca
CamBradford07 <152317249+CamBradford07@users.noreply.github.com>
2024-01-31 14:08:52 +0000

    Started the foundations of making the game

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,38 @@ public class Main {
         frame.setSize(500,500);
         JFrame applyNames = new JFrame("Enter Names");
         applyNames.setSize(500,500);
+      JFrame game = new JFrame("Play Yahtzee");
+      game.setSize(750,1000);
         
       
         JPanel panel = new JPanel(new GridLayout(3,1));
         JPanel getNames = new JPanel(new GridLayout(6,1));
+        JPanel playGame = new JPanel(new GridLayout(12,17));
 
         JLabel askPlayerCount = new JLabel("How many computers do you want to play against? 1-3");
         JTextField getPlayerCount = new JTextField();
         JButton submit = new JButton("Submit");
 
+      JLabel text = new JLabel("Enter names");
+      
       JTextField player1Name = new JTextField("Your Name");
       JTextField player2Name = new JTextField("Computer 1");
       JTextField player3Name = new JTextField("Computer 2");
       JTextField player4Name = new JTextField("Computer 3");
       JTextField[] nameFields = {player1Name, player2Name, player3Name, player4Name};
+
+      JLabel play1Name = new JLabel("");
+      JLabel play1Bonus = new JLabel("Bonus: ");
+      JLabel play1Ones = new JLabel("Ones: ");
+      JLabel play1Twos = new JLabel("Twos: ");
+      JLabel play1Threes = new JLabel("Threes: ");
+      JLabel play1Fours = new JLabel("FOurs: ");
+      JLabel play1Fives = new JLabel("Fives: ");
+      JLabel play1Sixes = new JLabel("Sixes: ");
+      JLabel play1SmallStraight = new JLabel("small Straight: ");
+      JLabel play1LargeStraight = new JLabel("Large Straight: ");
+
+      JButton submitNames = new JButton("Submit names");
       
         submit.addActionListener(new ActionListener() {
             @Override
@@ -35,7 +53,12 @@ public class Main {
                             askPlayerCount.setText("Invalid Number try again, pick 1-3");
                         }
                         else{
-                            
+                            getNames.add(text);
+                          for(int i = 0; i <= playercount;i++){
+                              getNames.add(nameFields[i]);
+                              }
+                            getNames.add(submitNames);
+                            applyNames.add(getNames);
                             frame.setVisible(false);
                             applyNames.setVisible(true);
                         }
@@ -48,6 +71,8 @@ public class Main {
             }
         });
 
+        
+      
         panel.add(askPlayerCount);
         panel.add(getPlayerCount);
         panel.add(submit);
