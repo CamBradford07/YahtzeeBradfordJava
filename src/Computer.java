9c556a733188b341a5fb910f27a70c18380c46a1
CamBradford07 <152317249+CamBradford07@users.noreply.github.com>
2024-02-01 15:45:25 +0000

    Continued work on computer class: seperate turn method

--- src/Computer.java
+++ src/Computer.java
@@ -15,6 +15,12 @@ public class Computer {
     private boolean yahtzeeDone;
     private int upperScore;
     private int totalScore;
+    private int oneCount;
+    private int twoCount;
+    private int threeCount;
+    private int fourCount;
+    private int fiveCount;
+    private int sixCount;
     private int[] roll = {0,0,0,0,0};
     public Computer(String name){
         this.name = name;
@@ -42,12 +48,13 @@ public class Computer {
         for(int i = 9; i < roll.length; i++) {
             roll[i] = (int) (Math.random() * 6 + 1);
         }
-        int oneCount = 0;
-        int twoCount = 0;
-        int threeCount = 0;
-        int fourCount = 0;
-        int fiveCount = 0;
-        int sixCount  = 0;
+         oneCount = 0;
+         twoCount = 0;
+         threeCount = 0;
+         fourCount = 0;
+         fiveCount = 0;
+        sixCount  = 0;
+          boolean fh = checkFullHouse();
         for(int x:roll){
             if(x == 1){oneCount++;}
             else if(x == 2){twoCount++;}
@@ -56,7 +63,7 @@ public class Computer {
            else if(x == 5){fiveCount++;}
            else{sixCount++;}
         }
-        if((oneCount == 5 || twoCount == 5 || threeCount == 5 || fourCount == 5 || fiveCount == 5 || sixCount == 5) && yahtzeeDone == false){
+        if((oneCount == 5 || twoCount == 5 || threeCount == 5 || fourCount == 5 || fiveCount == 5 || sixCount == 5)){
             yahtzeeDone = true;
             totalScore += 50;
         }
@@ -68,19 +75,222 @@ public class Computer {
             smallStraightDone = true;
             totalScore += 30;
         }
-
-
+          else if(fh == true && fullHouseDone == false){
+            fullHouseDone = true;
+            totalScore += 25;
+          }
+        else if((oneCount >= 4 || twoCount >= 4 || threeCount >=4 || fourCount >= 4 || fiveCount >= 4 || sixCount >= 4) && fourKindDone == false){
+          fourKindDone = true;
+          for(int x: roll){
+            totalScore += x;
+          }
+        }
+          else if((oneCount >= 3 || twoCount >= 3 || threeCount >=3 ||fourCount >= 3 || fiveCount >= 3 || sixCount >= 3) && fourKindDone == false){
+            fourKindDone = true;
+            for(int x: roll){
+              totalScore += x;
+            }
+          }
+          else if(sixCount >= 3 && sixesDone == false){
+            sixesDone = true;
+            totalScore += (6 * sixCount);
+            upperScore += (6 * sixCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(fiveCount >= 3 && fivesDone == false){
+            fivesDone = true;
+            totalScore += (5 * fiveCount);
+            upperScore += (5 * fiveCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(fourCount >= 3 && foursDone == false){
+            foursDone = true;
+            totalScore += (4 * fourCount);
+            upperScore += (4 * fourCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(threeCount >= 3 && threesDone == false){
+            threesDone = true;
+            totalScore += (3 * threeCount);
+            upperScore += (3 * threeCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(twoCount >= 3 && twosDone == false){
+            twosDone = true;
+            totalScore += (2 * twoCount);
+            upperScore += (2 * twoCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(oneCount >= 3 && onesDone == false){
+            onesDone = true;
+            totalScore += (1 * oneCount);
+            upperScore += (1 * oneCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+        else if(rollcount == 3){
+          if(chanceDone == false){
+            chanceDone = true;
+            for(int x:roll){
+              totalScore += x;
+            }
+          }
+          if(oneCount > 0 && onesDone == false){
+            onesDone = true;
+            totalScore += (1 * oneCount);
+            upperScore += (1 * oneCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(twoCount > 0 && twosDone == false){
+              twosDone = true;
+              totalScore += (2 * twoCount);
+              upperScore += (2 * twoCount);
+              if(checkBonus() == true){
+                totalScore += 35;
+              }
+            }
+          else if(threeCount > 0 && threesDone == false){
+            threesDone = true;
+            totalScore += (3 * threeCount);
+            upperScore += (3 * threeCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(fourCount > 0 && foursDone == false){
+            foursDone = true;
+            totalScore += (4 * fourCount);
+            upperScore += (4 * fourCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(fiveCount > 0 && fivesDone == false){
+            fivesDone = true;
+            totalScore += (5 * fiveCount);
+            upperScore += (5 * fiveCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+          else if(sixCount > 0 && sixesDone == false){
+            sixesDone = true;
+            totalScore += (6 * sixCount);
+            upperScore += (6 * sixCount);
+            if(checkBonus() == true){
+              totalScore += 35;
+            }
+          }
+        }
+          
         try {
                     Thread.sleep(1000);
                 }
         catch (Exception e) {
                     System.out.println(e);
                 }
-
+          rollcount++;
         }
     }
 
+private boolean checkFullHouse(){
+  boolean hasFullHouse = false;
+  if(oneCount == 3){
+    if(twoCount == 2){hasFullHouse = true;}
+    else if(threeCount == 2){hasFullHouse = true;}
+    else if(fourCount == 2){hasFullHouse = true;}
+    else if(fiveCount == 2){hasFullHouse = true;}
+    else if(sixCount == 2){hasFullHouse = true;}
+  }
+  else if(twoCount == 3){
+    if(oneCount == 2){hasFullHouse = true;}
+    else if(threeCount == 2){hasFullHouse = true;}
+    else if(fourCount == 2){hasFullHouse = true;}
+    else if(fiveCount == 2){hasFullHouse = true;}
+    else if(sixCount == 2){hasFullHouse = true;}
+  }
+  else if(threeCount == 3){
+    if(twoCount == 2){hasFullHouse = true;}
+    else if(oneCount == 2){hasFullHouse = true;}
+    else if(fourCount == 2){hasFullHouse = true;}
+    else if(fiveCount == 2){hasFullHouse = true;}
+    else if(sixCount == 2){hasFullHouse = true;}
+  }
+  else if(fourCount == 3){
+    if(twoCount == 2){hasFullHouse = true;}
+    else if(threeCount == 2){hasFullHouse = true;}
+    else if(oneCount == 2){hasFullHouse = true;}
+    else if(fiveCount == 2){hasFullHouse = true;}
+    else if(sixCount == 2){hasFullHouse = true;}
+  }
+  else if(fiveCount == 3){
+    if(twoCount == 2){hasFullHouse = true;}
+    else if(threeCount == 2){hasFullHouse = true;}
+    else if(fourCount == 2){hasFullHouse = true;}
+    else if(oneCount == 2){hasFullHouse = true;}
+    else if(sixCount == 2){hasFullHouse = true;}
+  }
+  else if(sixCount == 3){
+    if(twoCount == 2){hasFullHouse = true;}
+    else if(threeCount == 2){hasFullHouse = true;}
+    else if(fourCount == 2){hasFullHouse = true;}
+    else if(fiveCount == 2){hasFullHouse = true;}
+    else if(oneCount == 2){hasFullHouse = true;}
+  }
+  return hasFullHouse;
+}
+
+public boolean checkWin(){
+  boolean[] dones = {onesDone, twosDone, threesDone, foursDone, fivesDone, sixesDone, threeKindDone, fourKindDone, fullHouseDone, yahtzeeDone, chanceDone, smallStraightDone, largeStraightDone};
+boolean winner = true;
+  for(boolean x: dones){
+    if(x == false){
+      winner = false;
+      break;
+    }
+  }
+  return winner;
+}
+
+private boolean checkBonus(){
+boolean[] upperDone = {onesDone, twosDone, threesDone, foursDone, fivesDone, sixesDone};
+  boolean allDone = true;
+  for(boolean x: upperDone){
+    if(x == false){
+      allDone = false;
+      break;
+    }
+  }
+if(allDone == true && upperScore >= 63){
+  return true;
+}  
+  else{
+    return false;
+  }
+}
+
+public String getName(){
+return name;
+}
 
 
 
+
+
+
+
+  
 }
